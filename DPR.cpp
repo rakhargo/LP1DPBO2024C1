ed9c2e9317e0794135e40e700fcb7d8e6652102b
rakhargo <117525800+rakhargo@users.noreply.github.com>
2024-02-08 02:35:41 +0700

    Add files via upload

--- DPR.cpp
+++ DPR.cpp
@@ -3,19 +3,20 @@
 
 using namespace std;
 
-class DPR
+class DPR // membuat class
 {
-    private:
-        int id;
-        string nama;
-        string namaBidang;
-        string namaPartai;
+    private: // atribut private
+        int id; // untuk id anggota dpr
+        string nama; // untuk nama anggota dpr
+        string namaBidang; // untuk bidang anggota dpr
+        string namaPartai; // untuk partai anggota dpr
     
     public:
         // konstruktor
         DPR()
         {
-            this->id = 0;
+            // inisialisasi untuk atribut dpr
+            this->id = 0;  
             this->nama = "";
             this->namaBidang = "";
             this->namaPartai = "";
@@ -24,53 +25,54 @@ class DPR
         // konstruktor parameter
         DPR(int id, string nama, string namaBidang, string namaPartai)
         {
+            // mengisi nilai dari atribut menggunakan parameter yang ada
             this->id = id;
             this->nama = nama;
             this->namaBidang = namaBidang;
             this->namaPartai = namaPartai;
         }
 
-        int getId()
+        int getId() // mendapatkan id anggota dpr
         {
             return this->id; 
         }
 
-        string getNama()
+        string getNama() // mendapatkan nama anggota dpr
         {
             return this->nama; 
         }
 
-        string getBidang()
+        string getBidang() // mendapatkan bidang anggota dpr
         {
             return this->namaBidang; 
         }
 
-        string getPartai()
+        string getPartai() // mendapatkan partai anggota dpr
         {
             return this->namaPartai; 
         }
         
-        void setId(int id)
+        void setId(int id) // mengset id anggota dpr 
         {
             this->id = id;
         }
         
-        void setNama(string nama)
+        void setNama(string nama) // mengset nama anggota dpr
         {
             this->nama = nama;
         }
         
-        void setBidang(string bidang)
+        void setBidang(string bidang) // mengset bidang anggota dpr
         {
             this->namaBidang = bidang;
         }
         
-        void setPartai(string partai)
+        void setPartai(string partai) // mengset partai anggota dpr
         {
             this->namaPartai = partai;
         }
 
-        ~DPR()
+        ~DPR() // destruktor
         {
 
         }
--- Main.cpp
+++ Main.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 int main()
 {
-    int pilihMenu;
-    vector<DPR> listDPR;
+    int pilihMenu; // variabel pemilih menu untuk CRUD
+    vector<DPR> listDPR; // membuat struktur data vector untuk list anggota DPR
 
-    do
+    do // perulangan utama program untuk menu
     {
+        // pemilihan menu
         cout << endl << "Pilih Menu: " << endl;
         cout << "1. Add" << endl;
         cout << "2. Edit" << endl;
@@ -17,64 +18,64 @@ int main()
         cout << "4. View" << endl;
         cout << "Ketik 0 untuk mengakhiri program" << endl;
 
-        cin >> pilihMenu;
-        if (pilihMenu == 1)
+        cin >> pilihMenu; // meminta masukan nomor menu
+        if (pilihMenu == 1) // jika angka 1
         {
-            // add
-            int id; string nama, bidang, partai;
+            // maka melakukan tambah data anggota
+            int id; string nama, bidang, partai; // deklarasi variabel yang digunakan sebagai temp untuk setiap atribut anggota dpr 
 
-            cout << "ID: "; cin >> id;
-            cout << "Nama: "; cin >> nama;
-            cout << "Bidang: "; cin >> bidang;
-            cout << "Partai: "; cin >> partai;
+            cout << "ID: "; cin >> id; // meminta masukan id
+            cout << "Nama: "; cin >> nama; // meminta masukan nama
+            cout << "Bidang: "; cin >> bidang; // meminta masukan bidang
+            cout << "Partai: "; cin >> partai; // meminta masukan partai
 
-            DPR temp(id, nama, bidang, partai);
+            DPR temp(id, nama, bidang, partai); // instansiasi objek langsung menggunakan konstruktor parameter
             
-            listDPR.push_back(temp);
+            listDPR.push_back(temp); // memasukkan ke vector
             cout << "Data berhasil ditambah" << endl;
         }
-        else if (pilihMenu == 2)
+        else if (pilihMenu == 2) // jika angka 2
         {
-            // edit
-            int stop = 0;
-            do
+            // maka melakukan edit data anggota
+            int stop = 0; // variabel untuk stop dalam do while yang dimana digunakan untuk error handling saja
+            do // perulangan untuk error handling jika memilih id yang tidak ada di data
             {
-                int id;
-                cout << "ID yang ingin diedit: "; cin >> id;
-                int i;
-                for (i = 0; i < listDPR.size() && stop == 0; i++)
+                int id; // id yang ingin dicari
+                cout << "ID yang ingin diedit: "; cin >> id; // meminta masukan untuk id yang ingin diedit
+                int i; // indeks vector untuk mencari yang ketemu
+                for (i = 0; i < listDPR.size() && stop == 0; i++) // sequential search
                 {
-                    if (id == listDPR[i].getId())
+                    if (id == listDPR[i].getId()) // jika ketemu
                     {
-                        stop = 1;
+                        stop = 1; // maka stop dan keluar perulangan sehingga indeks tersimpan
                     }
                 }
 
-                if (stop == 1)
+                if (stop == 1) // jika id ketemu
                 {
                     cout << "Ditemukan ID " << id << endl;
 
                     cout << "Mengubah data" << endl;
 
-                    int id; string nama, bidang, partai;
-                    cout << "ID: "; cin >> id;          listDPR[i - 1].setId(id);
+                    int id; string nama, bidang, partai; // deklarasi variabel yang digunakan sebagai temp untuk setiap atribut anggota dpr 
+                    cout << "ID: "; cin >> id;          listDPR[i - 1].setId(id); // melakukan edit setiap atribut data
                     cout << "Nama: "; cin >> nama;      listDPR[i - 1].setNama(nama);
                     cout << "Bidang: "; cin >> bidang;  listDPR[i - 1].setBidang(bidang);
                     cout << "Partai: "; cin >> partai;  listDPR[i - 1].setPartai(partai);
                     cout << "Data berhasil diubah" << endl;
                 }
-                else
+                else // error handling
                 {
                     cout << "Tidak ada data dengan ID yang dimaksud" << endl;
                 }
                 
-            } while (stop == 0);
+            } while (stop == 0); // selama tidak ketemu
             
         }
-        else if (pilihMenu == 3)
+        else if (pilihMenu == 3) // jika pilih angka 3
         {
-            // del
-            int stop = 0;
+            // maka melakukan penghapusan data anggota
+            int stop = 0; // sama seperti edit tadi
             do
             {
                 int id;
@@ -88,34 +89,34 @@ int main()
                     }
                 }
 
-                if (stop == 1)
+                if (stop == 1) // hanya beda disini
                 {
-                    cout << "Ditemukan ID " << id << endl;
-                    listDPR.erase(listDPR.begin() + i);
+                    cout << "Ditemukan ID " << id << endl; // menemukan id yang dimaksud
+                    listDPR.erase(listDPR.begin() + i); // melakukan penghapusan id di indeks tersebut
                     cout << "Data berhasil dihapus" << endl;
                 }
-                else
+                else // error handling
                 {
                     cout << "Tidak ada data dengan ID yang dimaksud" << endl;
                 }
                 
-            } while (stop == 0);
+            } while (stop == 0); // selama belum ketemu
         }
-        else if (pilihMenu == 4)
+        else if (pilihMenu == 4) // jika pilih angka 4
         {
-            // view
-            if (listDPR.size() == 0)
+            // melihat setiap data anggota dpr
+            if (listDPR.size() == 0) // jika vector size nya 0
             {
                 cout << "Data kosong." << endl;
             }
             
-            for (int i = 0; i < listDPR.size(); i++)
+            for (int i = 0; i < listDPR.size(); i++) // melakukan perulangan untuk print all
             {
                 cout << i + 1 << ". " << listDPR[i].getId() << ' ' << listDPR[i].getNama() << ' ' << listDPR[i].getBidang() << ' ' << listDPR[i].getPartai() << endl;
             }
         }
         
-    } while (pilihMenu != 0);
+    } while (pilihMenu != 0); // selama tidak pilih 0
     
     cout << "Terima kasih telah menggunakan program ini!" << endl;
     return 0;
